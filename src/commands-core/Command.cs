0f5efc649ca8dbb0a1f842fa3cb58e99ec224690
larrykirschner <larrykirschner@gmail.com>
2017-09-09 18:22:24 -0700

    0.0.6

--- src/commands-core/Command.cs
+++ src/commands-core/Command.cs
@@ -4,6 +4,9 @@ namespace BeatThat.App
 {
 	public interface Command : Command<object> {}
 
+	/// <summary>
+	/// A <c>Command</c> encapsulates a single executable behaviour
+	/// </summary>
 	public interface Command<ArgType>
 	{
 		void Execute(ArgType n);
